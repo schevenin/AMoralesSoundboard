067d349c8264748c50dc4279415aaa021f1bb461
Rogelio Schevenin Jr <schevenin11@gmail.com>
2018-10-22 22:38:22 -0700

    v1

--- Guatemalan Embassy/AppDelegate.swift
+++ Guatemalan Embassy/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Guatemalan Embassy
 //
-//  Created by Rogelio Schevenin on 3/27/18.
-//  Copyright © 2018 Rogelio Schevenin. All rights reserved.
+//  Created by Rogelio Schevenin on 10/21/18.
+//  Copyright © 2018 ScheveninDevOrg. All rights reserved.
 //
 
 import UIKit
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
--- Guatemalan Embassy/ViewController.swift
+++ Guatemalan Embassy/ViewController.swift
@@ -12,108 +12,85 @@ import AVFoundation
 class ViewController: UIViewController {
     
     func stopAudio() {
-        audioPlayer1.stop()
-        audioPlayer2.stop()
-        audioPlayer3.stop()
-        audioPlayer4.stop()
-        audioPlayer5.stop()
-        audioPlayer6.stop()
+        audioPlayerEmbassy.stop()
+        audioPlayerChisme.stop()
+        audioPlayerPapas.stop()
+        audioPlayerEnamorado.stop()
+        audioPlayerMijo.stop()
     }
     
-    @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var copyright: UILabel!
-    @IBOutlet weak var morales: UILabel!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var label4: UILabel!
-    @IBOutlet weak var label5: UILabel!
-    @IBOutlet weak var label6: UILabel!
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
-    @IBOutlet weak var button4: UIButton!
-    @IBOutlet weak var button5: UIButton!
-    @IBOutlet weak var button6: UIButton!
-    @IBAction func button1(_ sender: UIButton) {
+    @IBOutlet weak var buttonEmbassy: UIButton!
+    @IBOutlet weak var buttonChisme: UIButton!
+    @IBOutlet weak var buttonPapas: UIButton!
+    @IBOutlet weak var buttonEnamorado: UIButton!
+    @IBOutlet weak var buttonMijo: UIButton!
+    @IBOutlet weak var buttonStop: UIButton!
+    
+    @IBAction func buttonStop(_ sender: UIButton) {
         stopAudio()
-        audioPlayer1.play()
     }
-    @IBAction func button2(_ sender: UIButton) {
-        stopAudio()
-        audioPlayer2.play()
+    @IBAction func buttonEmbassy(_ sender: UIButton) {
+        audioPlayerEmbassy.play()
     }
-    @IBAction func button3(_ sender: UIButton) {
-        stopAudio()
-        audioPlayer3.play()
+    @IBAction func buttonChisme(_ sender: UIButton) {
+        audioPlayerChisme.play()
     }
-    @IBAction func button4(_ sender: UIButton) {
-        stopAudio()
-        audioPlayer4.play()
+    @IBAction func buttonPapas(_ sender: UIButton) {
+        audioPlayerPapas.play()
     }
-    @IBAction func button5(_ sender: UIButton) {
-        stopAudio()
-        audioPlayer5.play()
+    @IBAction func buttonEnamorado(_ sender: UIButton) {
+        audioPlayerEnamorado.play()
     }
-    @IBAction func button6(_ sender: UIButton) {
-        stopAudio()
-        audioPlayer6.play()
+    @IBAction func buttonMijo(_ sender: UIButton) {
+        audioPlayerMijo.play()
     }
-    var audioPlayer1 : AVAudioPlayer!
-    var audioPlayer2 : AVAudioPlayer!
-    var audioPlayer3 : AVAudioPlayer!
-    var audioPlayer4 : AVAudioPlayer!
-    var audioPlayer5 : AVAudioPlayer!
-    var audioPlayer6 : AVAudioPlayer!
+    var audioPlayerEmbassy : AVAudioPlayer!
+    var audioPlayerChisme : AVAudioPlayer!
+    var audioPlayerPapas : AVAudioPlayer!
+    var audioPlayerEnamorado : AVAudioPlayer!
+    var audioPlayerMijo : AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+                
+        let urlEmbassy = Bundle.main.url(forResource: "embassy", withExtension: "mp3")
+        let urlChisme = Bundle.main.url(forResource: "chisme", withExtension: "mp3")
+        let urlPapas = Bundle.main.url(forResource: "papas", withExtension: "mp3")
+        let urlEnamorado = Bundle.main.url(forResource: "enamorado", withExtension: "mp3")
+        let urlMijo = Bundle.main.url(forResource: "mijo", withExtension: "mp3")
         
-        let url1 = Bundle.main.url(forResource: "1", withExtension: "mp3")
-        let url2 = Bundle.main.url(forResource: "2", withExtension: "mp3")
-        let url3 = Bundle.main.url(forResource: "3", withExtension: "mp3")
-        let url4 = Bundle.main.url(forResource: "4", withExtension: "mp3")
-        let url5 = Bundle.main.url(forResource: "5", withExtension: "mp3")
-        let url6 = Bundle.main.url(forResource: "6", withExtension: "mp3")
-        
         do {
-            audioPlayer1 = try AVAudioPlayer(contentsOf: url1!)
-            audioPlayer1.prepareToPlay()
-        
-        }catch let error as NSError {
-            print(error.debugDescription)
-        }
-        do {
-            audioPlayer2 = try AVAudioPlayer(contentsOf: url2!)
-            audioPlayer2.prepareToPlay()
+            audioPlayerEmbassy = try AVAudioPlayer(contentsOf: urlEmbassy!)
+            audioPlayerEmbassy.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
         }
         do {
-            audioPlayer3 = try AVAudioPlayer(contentsOf: url3!)
-            audioPlayer3.prepareToPlay()
+            audioPlayerChisme = try AVAudioPlayer(contentsOf: urlChisme!)
+            audioPlayerChisme.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
         }
         do {
-            audioPlayer4 = try AVAudioPlayer(contentsOf: url4!)
-            audioPlayer4.prepareToPlay()
+            audioPlayerPapas = try AVAudioPlayer(contentsOf: urlPapas!)
+            audioPlayerPapas.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
         }
         do {
-            audioPlayer5 = try AVAudioPlayer(contentsOf: url5!)
-            audioPlayer5.prepareToPlay()
+            audioPlayerEnamorado = try AVAudioPlayer(contentsOf: urlEnamorado!)
+            audioPlayerEnamorado.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
         }
         do {
-            audioPlayer6 = try AVAudioPlayer(contentsOf: url6!)
-            audioPlayer6.prepareToPlay()
+            audioPlayerMijo = try AVAudioPlayer(contentsOf: urlMijo!)
+            audioPlayerMijo.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
