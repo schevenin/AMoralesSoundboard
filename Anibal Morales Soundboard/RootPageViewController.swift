1d519a00a52bef28516f91f139315d629128a645
Rogelio Schevenin Jr <schevenin11@gmail.com>
2019-01-14 20:54:18 -0800

    v1.4.1

---
+++ Anibal Morales Soundboard/RootPageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  RootPageViewController.swift
+//  Anibal Morales Soundboard
+//
+//  Created by Rogelio Schevenin on 1/11/19.
+//  Copyright © 2019 ScheveninDevOrg. All rights reserved.
+//
+
+import UIKit
+
+class RootPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var viewControllerList:[UIViewController] = {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc1 = sb.instantiateViewController(withIdentifier: "PageOne")
+        let vc2 = sb.instantiateViewController(withIdentifier: "PageTwo")
+        
+        return [vc1,vc2]
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        let previousIndex = vcIndex - 1
+        guard previousIndex >= 0 else {return nil}
+        guard viewControllerList.count > previousIndex else {return nil}
+        return viewControllerList[previousIndex]
+        
+        }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        let nextIndex = vcIndex + 1
+        guard viewControllerList.count != nextIndex else {return nil}
+        guard viewControllerList.count > nextIndex else {return nil}
+        return viewControllerList[nextIndex]
+        
+        }
+    
+}
--- Anibal Morales Soundboard/ViewController.swift
+++ Anibal Morales Soundboard/ViewController.swift
@@ -11,16 +11,30 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    let backgroundImageView = UIImageView()
+    
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundImageView.image = UIImage(named: "Wallpaper")
+        view.sendSubviewToBack(backgroundImageView)
+    }
+    
     func stopAudio() {
         audioPlayerGuatemalan.stop()
         audioPlayerChisme.stop()
         audioPlayerPapas.stop()
         audioPlayerEnamorado.stop()
         audioPlayerMijo.stop()
+        audioPlayerMartin.stop()
     }
     
-    @IBOutlet weak var copyright: UILabel!
     @IBOutlet weak var buttonGuatemalan: UIButton!
+    @IBOutlet weak var buttonMartin: UIButton!
     @IBOutlet weak var buttonChisme: UIButton!
     @IBOutlet weak var buttonPapas: UIButton!
     @IBOutlet weak var buttonEnamorado: UIButton!
@@ -32,6 +46,9 @@ class ViewController: UIViewController {
     }
     @IBAction func buttonGuatemalan(_ sender: UIButton) {
         audioPlayerGuatemalan.play()
+    }
+    @IBAction func buttonMartin(_ sender: UIButton) {
+        audioPlayerMartin.play()
     }
     @IBAction func buttonChisme(_ sender: UIButton) {
         audioPlayerChisme.play()
@@ -45,24 +62,35 @@ class ViewController: UIViewController {
     @IBAction func buttonMijo(_ sender: UIButton) {
         audioPlayerMijo.play()
     }
+    
     var audioPlayerGuatemalan : AVAudioPlayer!
     var audioPlayerChisme : AVAudioPlayer!
     var audioPlayerPapas : AVAudioPlayer!
     var audioPlayerEnamorado : AVAudioPlayer!
     var audioPlayerMijo : AVAudioPlayer!
+    var audioPlayerMartin: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+
+        
         let urlGuatemalan = Bundle.main.url(forResource: "guatemalan", withExtension: "mp3")
         let urlChisme = Bundle.main.url(forResource: "chisme", withExtension: "mp3")
         let urlPapas = Bundle.main.url(forResource: "papas", withExtension: "mp3")
         let urlEnamorado = Bundle.main.url(forResource: "enamorado", withExtension: "mp3")
         let urlMijo = Bundle.main.url(forResource: "mijo", withExtension: "mp3")
+        let urlMartin = Bundle.main.url(forResource: "martin", withExtension: "mp3")
         
         do {
             audioPlayerGuatemalan = try AVAudioPlayer(contentsOf: urlGuatemalan!)
             audioPlayerGuatemalan.prepareToPlay()
+            
+        }catch let error as NSError {
+            print(error.debugDescription)
+        }
+        do {
+            audioPlayerMartin = try AVAudioPlayer(contentsOf: urlMartin!)
+            audioPlayerMartin.prepareToPlay()
             
         }catch let error as NSError {
             print(error.debugDescription)
